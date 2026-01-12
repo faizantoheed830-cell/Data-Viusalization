a3bbcf64c5faea0e952712cb13fb5a09e5c83758
Faizan Tauheed <faizantoheed830@gmail.com>
2026-01-12 11:27:31 +0500

    Add files via upload

--- Data Viusalization/mpl_squares.py
+++ Data Viusalization/mpl_squares.py
@@ -1,15 +1,15 @@
 import matplotlib.pyplot as plt
 
 plt.style.use('seaborn-v0_8')
-input_values = [1,2,3,4,5]
+x_values = [1,2,3,4,5]
 
-squares = [1,4,9,16,25]
+y_values = [1,4,9,16,25]
 
 
 fig , ax = plt.subplots()  # Fig Variable represents the collection of plots in a label & ax variable each plot
 
-ax.plot(input_values , squares , linewidth = 3)
-ax.scatter(2,4, s=200)
+ax.plot(x_values , y_values , linewidth = 3)
+ax.scatter(x_values,y_values, s=100)
 ax.set_title("Square Numbers" , fontsize = 24)
 ax.set_xlabel("Value" , fontsize = 14)
 ax.set_ylabel("Square of value" , fontsize = 14)
