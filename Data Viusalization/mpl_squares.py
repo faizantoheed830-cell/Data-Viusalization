7a1ea588b232d476d6b13808c2b1a051d2b2bfa5
Faizan Tauheed <faizantoheed830@gmail.com>
2026-01-12 11:24:09 +0500

    Add files via upload

--- Data Viusalization/mpl_squares.py
+++ Data Viusalization/mpl_squares.py
@@ -9,7 +9,7 @@ squares = [1,4,9,16,25]
 fig , ax = plt.subplots()  # Fig Variable represents the collection of plots in a label & ax variable each plot
 
 ax.plot(input_values , squares , linewidth = 3)
-
+ax.scatter(2,4, s=200)
 ax.set_title("Square Numbers" , fontsize = 24)
 ax.set_xlabel("Value" , fontsize = 14)
 ax.set_ylabel("Square of value" , fontsize = 14)
